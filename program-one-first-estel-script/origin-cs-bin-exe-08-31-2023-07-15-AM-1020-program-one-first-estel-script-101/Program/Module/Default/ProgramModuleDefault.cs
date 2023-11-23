f8a1243e105071585d30a919f512d54dc948aa4e
Pepo Pepev <135320714+segmentar@users.noreply.github.com>
2023-11-23 21:09:29 +0200

    change

---
+++ program-one-first-estel-script/origin-cs-bin-exe-08-31-2023-07-15-AM-1020-program-one-first-estel-script-101/Program/Module/Default/ProgramModuleDefault.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+
+using Core.DimensionTwoSecondSecondaryII.ConsoleLJ;
+
+namespace Core
+{
+    using System;
+
+    public partial struct ProgramModule
+    {
+        public static ProgramModule ProgramModuleDefault()
+        {
+            Program program;
+
+            program = Program.MakeProgramDefaultSurface();
+
+            if (ArchitectureConsoleLJOneFirst.IsExternalCall is false)
+            {
+                program.ConsoleSetup();
+            }
+            else
+                "false".ToString();
+
+
+            ProgramModule module;
+
+            module = new ProgramModule();
+
+            module.Program = program;
+
+            module = ProgramModuleFunctionOne(module);
+
+            module = ProgramModuleFunctionTwo(module);
+
+            module = ProgramModuleFunctionThree(module);
+
+            module = ProgramModuleFunctionFour(module);
+
+            return module;
+        }
+    }
+}
